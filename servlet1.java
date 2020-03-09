51aded97f879bd803cb49dcdf319a486659b912d
nada-mannai <61947651+nada-mannai@users.noreply.github.com>
2020-03-09 14:05:29 +0100

    Add files via upload

---
+++ servlet1.java
@@ -0,0 +1,46 @@
+package isetj.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class servlet1
+ */
+@WebServlet("/servlet1")
+public class servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	response.getWriter().println("<html><head><title> Servlet de Bienvenue </title></head>");
+	response.getWriter().println("<body><h3><font color='blue'>");
+	response.getWriter().println("Message de bienvenue affiché par le servlet 1");
+	response.getWriter().println("</font></h3></body></html>");
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
